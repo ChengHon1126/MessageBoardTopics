cc2f0c5e0e4d2f33378e84d8aa4d67b701dc0bfd
cheng <lkmd555@gmail.com>
2023-05-31 22:33:03 +0800

    新增會員註冊與登入，並留言。

--- index.js
+++ index.js
@@ -31,10 +31,14 @@ app.use(express.urlencoded({extended:true}));
 app.get("/" , function(req,res){
     res.render("signin.ejs");
 })
-app.get("/signon", function(req, res){
-    res.render("signon.ejs")
+app.get("/signup", function(req, res){
+    res.render("signup.ejs")
 })
 app.get("/message",async function(req,res){
+    if(!req.session.user){
+        res.redirect("/");
+        return;
+    }
     const collection = db.collection("message");
     const result =await collection.find({});
     let data = [];
@@ -50,25 +54,53 @@ app.get("/error", function(req,res){
 
 
 
-app.post("/signonTurn",async function(req, res){
+app.post("/signupTurn",async function(req, res){
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
     const collection = db.collection("user")
-    let result = collection.findOne({
+    let result =await collection.findOne({
         email:email
     });
     if(result !== null){
-        res.redirect("/error?msg=暱稱重複");
+        res.redirect("/error?msg=信箱重複");
         return;
     }
+    result =await collection.findOne({
+        name:name
+    });
+    if(result !== null){
+        res.redirect("/error?msg=信箱重複");
+        return;
+    }
+
     result =await collection.insertOne({
-        name:name,email:email,password:password
+        name:name,
+        email:email,
+        password:password
     });
     res.redirect("/")
 })
+app.post("/signinTurn",async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const collection = db.collection("user");
+    let result =await collection.findOne({
+        $and:[
+            {email:email},
+            {password:password}
+        ]
+    });
+    if(result === null){
+        res.redirect("/error?msg=帳號密碼錯誤。");
+        return;
+    }
+    req.session.user = result;
+    res.redirect("message");
+})
 
 app.get("/update",async function(req,res){
+    const name = req.session.user.name;
     const message = req.query.message;
     const collection = db.collection("message");
     if (message === ""){
@@ -77,6 +109,7 @@ app.get("/update",async function(req,res){
     }
 
     await collection.insertOne({
+        name:name,
         message:message
     });
     res.redirect("/message")
