66ce844785d91006ff897904661679f40f5cbc85
cheng <lkmd555@gmail.com>
2023-05-31 18:34:28 +0800

    add signin and signon

--- index.js
+++ index.js
@@ -27,7 +27,14 @@ app.set("view engin","ejs");
 app.set("views","./views");
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
-app.get("/",async function(req,res){
+//登入畫面
+app.get("/" , function(req,res){
+    res.render("signin.ejs");
+})
+app.get("/signon", function(req, res){
+    res.render("signon.ejs")
+})
+app.get("/message",async function(req,res){
     const collection = db.collection("message");
     const result =await collection.find({});
     let data = [];
@@ -41,22 +48,40 @@ app.get("/error", function(req,res){
     res.render("error.ejs",{msg:msg});
 });
 
-app.get("/update",async function(req,res){
-    const name = req.query.name;
-    const message = req.query.message;
-    const collection = db.collection("message");
-    let result = await collection.findOne({
-        name:name
+
+
+app.post("/signonTurn",async function(req, res){
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const collection = db.collection("user")
+    let result = collection.findOne({
+        email:email
     });
     if(result !== null){
         res.redirect("/error?msg=暱稱重複");
         return;
     }
     result =await collection.insertOne({
-        name:name,message:message
+        name:name,email:email,password:password
     });
     res.redirect("/")
 })
+
+app.get("/update",async function(req,res){
+    const message = req.query.message;
+    const collection = db.collection("message");
+    if (message === ""){
+        res.redirect("/error?msg=請輸入文字");
+        return;
+    }
+
+    await collection.insertOne({
+        message:message
+    });
+    res.redirect("/message")
+})
+
 app.listen(3000,function(){
     console.log("Server Started")
 })
